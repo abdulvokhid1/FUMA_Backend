71ca5708790d68428b909c608d1c12431274d72d
굳프렌즈 <ibrakhimov7747a@gmail.com>
2025-09-15 20:20:45 +0900

    feat: create account method created in user model

--- src/trading/trading.module.ts
+++ src/trading/trading.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TradingService } from './trading.service';
 import { TradingController } from './trading.controller';
-import { PrismaService } from 'prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
   providers: [TradingService, PrismaService],
