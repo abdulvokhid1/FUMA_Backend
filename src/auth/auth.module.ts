1c64bfdbbe2b1bb1cbdb72e84a2ba6ee1b0a0f77
굳프렌즈 <ibrakhimov7747a@gmail.com>
2025-11-25 15:39:59 +0900

    feat: done

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtStrategy } from '@/utils/jwt.strategy';
-import { GoogleStrategy } from './google-strategy';
+import { JwtStrategy } from '../utils/jwt.strategy';
+import { GoogleStrategy } from './google.strategy';
 import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
